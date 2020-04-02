e0abf7a347ba1c64d8bcd7457aed2a026b3312b8
dbeccaria826 <beccdaniel@gmail.com>
2020-04-02 17:26:30 -0400

    generating a typed list of skills instead of backspace effect

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ const about = {
     name: 'Daniel Beccaria',
     job: 'Web Developer',
     area: 'Philadelphia',
-    interests: ['reactjs', 'javascript', 'nodejs', 'html', 'css']
+    interests: ['javascript', 'html', 'css', 'reactjs', 'nodejs']
 };
 
 let i = 0;
@@ -16,7 +16,7 @@ const { name, job, area, interests } = about;
 let message = `<p class="m-top-6 big-text">Hi, my name is <span class="name">${name}</span>.
                 <br> I'm a <span class="name">${job.toLowerCase()}</span> 
                 based in <span class="name">${area}</span>.
-                <br></p><br><p class="p-text montserrat m-top-2">Currently building things with <span class="montserrat" id="interests">${interests[j]}</span></p>`;
+                <br></p><br><p class="p-text montserrat m-top-2">Currently building things with <br><span class="montserrat" id="interests">${interests.join('<br>  ')}</span></p>`;
 
 // Creating a keyboard type effect as an introduction
 
@@ -35,12 +35,12 @@ const typeEffectOnScreen = () => {
 
     //Recursion
 
-    // if (isTag) {
-    //     return typeEffectOnScreen()
-    // }
-    while (isTag) {
+    if (isTag) {
         return typeEffectOnScreen()
     }
+    // while (isTag) {
+    //     return typeEffectOnScreen()
+    // }
 
     if (currentCharacter === '.' || currentCharacter === '?') {
         setTimeout(typeEffectOnScreen, 500)
@@ -54,28 +54,34 @@ const typeEffectOnScreen = () => {
 // Creating a backspace cursor effect to cycle through my interests
 
 const backSpaceEffect = () => {
-    const character = interests[j] ? interests[j].slice(0, (i -= 1)) : "";
-    const interestText = document.querySelector('#interests');
-    interestText.innerHTML = `${character}<span class="blinky"></span>`;
-    if (character.length === 0) {
-        i = 0;
-        if (j < interests.length - 1) {
-            j++;
-        } else {
-            j = 0;
-        }
-        message = `<span class="montserrat">${interests[j]}</span>`
-        target = document.querySelector('#interests')
-        isAnimated = true;
-        setTimeout(typeEffectOnScreen, 200);
+    
+    for (let item in interests) {
+        const character = interests[item]
+        console.log(character)
+    }
 
-        return
-    }
-    if (character === interests[j]) {
-        setTimeout(backSpaceEffect, 200)
-    } else {
-        setTimeout(backSpaceEffect, 200)
-    }
+    const interestText = document.querySelector('#interests');
+    console.log(interestText)
+    // interestText.innerHTML = `${character}<span class="blinky"></span>`;
+    // if (character.length === 0) {
+    //     i = 0;
+    //     if (j < interests.length - 1) {
+    //         j++;
+    //     } else {
+    //         j = 0;
+    //     }
+    //     message = `<span class="montserrat">${interests[j]}</span>`
+    //     target = document.querySelector('#interests')
+    //     isAnimated = true;
+    //     setTimeout(typeEffectOnScreen, 200);
+
+    //     return
+    // }
+    // if (character === interests[j]) {
+    //     setTimeout(backSpaceEffect, 200)
+    // } else {
+    //     setTimeout(backSpaceEffect, 200)
+    // }
 };
 
 const showButtonAfter = () => {
