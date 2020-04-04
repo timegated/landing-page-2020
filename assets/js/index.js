1dba45c148c71ac10279a9f1327aabd2625af211
dbeccaria826 <beccdaniel@gmail.com>
2020-04-04 15:14:40 -0400

    adding more styling to create a vscode theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,7 @@ let i = 0;
 let target = document.querySelector('#typedIntro');
 let isTag;
 const { firstName, lastName, job, area, interests, navItems } = about;
-let message = `<pre class="p-text montserrat m-top-2">const person = {<br>      firstName: ${firstName},<br>      lastName: ${lastName},<br>      area: ${area}<br>};<br><br>const techStack = [${interests.join(',  ')}];<br><span class="container-nav-row">const navbar = [${navItems.join(',')}];</span></pre>`;
+let message = `<pre class="p-text montserrat m-top-2 js-string-color"><span class="js-const-color">const</span> <span class="js-var-color">person</span> = {<br>      <span class="js-var-color">firstName:</span> ${firstName},<br>      <span class="js-var-color">lastName:</span> ${lastName},<br>      <span class="js-var-color">area:</span> ${area}<br>};<br><br><span class="js-const-color">const</span> <span class="js-var-color">tech</span> = [${interests.join(',  ')}];<br><span class="container-nav-row"><span class="js-const-color">const</span> <span class="js-var-color"> navbar</span> = [${navItems.join(',')}];</span></pre>`;
 
 // Creating a keyboard type effect as an introduction
 
