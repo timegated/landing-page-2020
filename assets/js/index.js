6f7d8d7bd99c6d215ec4887a561680833dc3458e
dbeccaria826 <beccdaniel@gmail.com>
2020-04-03 23:09:48 -0400

    reworking intro

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,62 +3,57 @@ const about = {
     job: 'Web Developer',
     area: 'Philadelphia',
     interests: ['javascript', 'reactjs', 'nodejs'],
-    navItems: ['<a href="#about" class="nav-button">About</a>', '<a href="#projects" class="nav-button">Projects</a>', '<a href="assets/resume/DJBResume March 2020.pdf" class="nav-button">Resume</a>']
+    navItems: ['<a href="#about" class="nav-button">About</a>',
+        '<a href="#projects" class="nav-button">Projects</a>',
+        '<a href="assets/resume/DJBResume March 2020.pdf" target="_blank" class="nav-button">Resume</a>']
 };
+
+
+
+// Iterators
 
 let i = 0;
 let j = 0;
+
+// Elements
 let target = document.querySelector('#typedIntro');
-isAnimated = false;
+
 let isTag;
 
 const { name, job, area, interests, navItems } = about;
 console.log(navItems)
 
-let message = `<p class="m-top-6 big-text">Hi, my name is <span class="name">${name}</span>.
-                <br> I'm a <span class="name">${job.toLowerCase()}</span> 
-                based in <span class="name">${area}</span>.
-                <br></p><br>
-                <p class="p-text montserrat m-top-2">Currently building web apps with <br><span class="montserrat" id="interests"> const tech = [${interests.join(',  ')}];</span> 
-                <br><span class="montserrat container-nav-row">const navbar = [${navItems.join(',')}];</span></p>`;
+
+
+let message = `<p class="p-text montserrat m-top-2">const webDev = {<br>name: ${name}, <br> area: ${area}<br>}; <br><span class="montserrat" id="interests"> const techStack = [${interests.join(',  ')}];</span> <br><span class="montserrat container-nav-row">const navbar = [${navItems.join(',')}]</span>;`;
 
 // Creating a keyboard type effect as an introduction
 
 const typeEffectOnScreen = () => {
     const character = message.slice(0, i += 1);
-  
+    console.log(character)
     const currentCharacter = character.slice(-1);
+    console.log(currentCharacter)
 
     if (currentCharacter === '<') isTag = true;
     if (currentCharacter === '>') isTag = false;
 
-    //Recursion
+    // Recursion
 
     if (isTag) {
         return typeEffectOnScreen()
-    }
+    } 
 
-    if (currentCharacter === '.' || currentCharacter === '?') {
+    if (currentCharacter === ';') {
         setTimeout(typeEffectOnScreen, 500)
     } else {
-        setTimeout(typeEffectOnScreen, 25)
+       setTimeout(typeEffectOnScreen, 50)
     }
 
     target.innerHTML = `${character}<span class="blinky"></span>`;
 }
 
 
-
-
-
-const showButtonAfter = () => {
-    const button = document.querySelectorAll('.button');
-    button.forEach(button => {
-        button.classList.remove('hide')
-        return button.classList.add('fade-in')
-    })
-    
-    
-}
-setTimeout(showButtonAfter, 4000)
-typeEffectOnScreen()
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(typeEffectOnScreen, 100)
+})
