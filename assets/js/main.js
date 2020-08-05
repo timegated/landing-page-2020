51bffd8f4a4d9bb3a7a76a93a7deb72e105f6c23
dbeccaria826 <beccdaniel@gmail.com>
2020-08-05 18:13:38 -0400

    url: fixed url for requesting json data

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ import fetchProjects from './modules/fetchProjects.js';
 import handleScroll from './modules/scrollTo.js';
 
 
-fetchProjects('https://raw.githubusercontent.com/timegated/landing-page-2020/design-test/data.json').then((res) => {
+fetchProjects('https://raw.githubusercontent.com/timegated/landing-page-2020/master/data.json').then((res) => {
     container.projects = res.projects;
     handleScroll
     container.init();
