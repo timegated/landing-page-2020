96dcc8b1520f57da8738660a24bd418ee6664dfb
dbeccaria826 <beccdaniel@gmail.com>
2020-08-23 12:01:54 -0400

    added responsive profile picture

--- src/js/container.js
+++ src/js/container.js
@@ -24,7 +24,7 @@ const container = {
       summary: "Calculator project showcasing event delegation",
       image: eventCalc,
       github: "https://github.com/timegated/event-delecalculator",
-      stack: ["fab fa-html5", "fab fa-css3-alt" ],
+      stack: ["fab fa-html5", "fab fa-css3-alt"],
       deployed: "https://timegated.github.io/event-delecalculator/calc.html",
       year: 2019
     },
@@ -39,15 +39,16 @@ const container = {
     }
   ],
   targets: {
-      projectElement: document.getElementById('work'),
-      typedIntroElement: document.getElementById('typedIntro'),
-      navBar: document.querySelector('.navbar'),
-      sideBarButton: document.querySelectorAll('#button'),
+    projectElement: document.getElementById('work'),
+    profileElement: document.getElementById('profile'),
+    typedIntroElement: document.getElementById('typedIntro'),
+    navBar: document.querySelector('.navbar'),
+    sideBarButton: document.querySelectorAll('#button'),
   },
   generateProjects() {
-      this.targets.projectElement.innerHTML = `
+    this.targets.projectElement.innerHTML = `
       ${this.projects.map(function (project) {
-          return `
+      return `
           <section>
              <a href="${project.github}" class="image" target="_blank" rel="noopener noreferrer"><img src="${project.image}" alt="${project.title}" data-position="center center"></a>
             <div class="content">
@@ -55,22 +56,22 @@ const container = {
                 <h2>${project.title}</h2>
                 <p>${project.summary}</p>
                     ${project.stack ? project.stack.map(icon => {
-                      return `
+        return `
                   <i class="icon icon-dark ${icon}"></i>
                  `
-                    }).join('')
-            : console.log("nothing here yet")}
+      }).join('')
+          : console.log("nothing here yet")}
                             <a href="${project.deployed}" target="_blank" class="button primary large">Live</a>
 
               </div>
             </div>
           </section>
           `
-      }).join('')
+    }).join('')
       }
       `
   },
-  init () {
+  init() {
     this.generateProjects()
   }
 };
